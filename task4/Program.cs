d6919bf798e3a1551dbd0f9df213188d875b1275
mirawingin <mirawingin174@gmail.com>
2024-01-26 22:58:19 +0500

    comparing numbers with a true-false parameter

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 5;
+int c = 7;
+int d = 9;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+System.Console.WriteLine(max);
