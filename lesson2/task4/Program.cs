af215265b1e54f779545d31297d6b093ba1ae73b
mirawingin <mirawingin174@gmail.com>
2024-01-31 18:08:37 +0500

    Lesson2 add task4

---
+++ lesson2/task4/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = 5;
+int[] array = { 20, 3, 25, 8, 13 };
+int i = 0;
+int max = array[0];
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+
+System.Console.WriteLine(max);
