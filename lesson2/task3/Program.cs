95071a67aaeaecf0a5392297284f66a19406242f
mirawingin <mirawingin174@gmail.com>
2024-01-31 17:36:29 +0500

    Lesson2 add task3

---
+++ lesson2/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 3, 12, 9, 4, 45, 56, 31, 10, 2, 48 };
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
