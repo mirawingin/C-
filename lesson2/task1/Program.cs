cd5c0fc9b403239b779658ab9601d10b8925d486
mirawingin <mirawingin174@gmail.com>
2024-01-31 14:42:30 +0500

    lesson2 add task2 Methods of array creation

---
+++ lesson2/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = new int[5];
+arr[0] = 22;
+arr[1] = 21;
+arr[2] = 29;
+arr[3] = 45;
+arr[4] = 33;
+System.Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[1]);
+System.Console.WriteLine(arr[2]);
+System.Console.WriteLine(arr[3]);
+System.Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5] { 22, 26, 72, 31, 13 };
+
+int[] arr3 = (54, 45, 32, 23, 22);
