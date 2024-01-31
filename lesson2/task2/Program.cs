f8d8bd33f65a32c59542e36a719fd0594ba0460a
mirawingin <mirawingin174@gmail.com>
2024-01-31 16:00:21 +0500

    lesson2 add task2 another way to save the array output

---
+++ lesson2/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < n)
+{
+    arr[i] = i + 1;
+    //System.Console.Write(arr[i]);
+    //System.Console.Write(' ');
+    System.Console.Write($"{arr[i]} ");
+
+    i = i + 1;
+}
