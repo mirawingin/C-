7ff944d1928f24ed370c01f5bf04f97a61e794a8
mirawingin <mirawingin174@gmail.com>
2024-02-01 12:52:21 +0500

    lesson2 add task6

---
+++ lesson2/task6/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задание 1. Совместная работа
+// Семинар 3. Массивы
+// 15 мин
+// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да
+
+int[] array = { 1, 3, 4, 19, 3 };
+int number = 4;
+bool result = false;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == number)
+    {
+        result = true;
+        break;
+    }
+}
+if (result == true)
+{
+    System.Console.WriteLine("YES");
+}
+else
+{
+    System.Console.WriteLine("No");
+}
