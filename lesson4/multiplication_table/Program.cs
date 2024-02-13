df041059210fa6556f0cf176123e8019d28c4992
mirawingin <mirawingin174@gmail.com>
2024-02-13 17:20:49 +0500

    lesson4 add multiplication_table

---
+++ lesson4/multiplication_table/Program.cs
@@ -0,0 +1,21 @@
+﻿// таблица умножения c клавиатурным вводом
+int n = Convert.ToInt32(Console.ReadLine());
+int[, ] matrix = new int[n, n];
+for(int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+    }
+    System.Console.WriteLine();
+}
+for(int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        System.Console.Write(matrix[i, j]);
+        System.Console.Write("\t");
+    }
+    System.Console.WriteLine();
+}
